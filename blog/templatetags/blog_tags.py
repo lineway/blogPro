52f056786b56b57ea70144dc6042e3c2e84f2efb
zhangyiming <zhangyiming07@163.com>
2018-05-06 23:13:52 +0800

    文章增加标签

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -22,4 +22,4 @@ def get_categories():
 
 @register.simple_tag
 def get_tags():
-    return Tag.objects.annotate(num_posts=('Count')).filter(num_posts__gt=0)
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
