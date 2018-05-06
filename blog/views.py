f9a0a65d84315941e5d3f1f2f28dd0bd474d5fef
zhangyiming <zhangyiming07@163.com>
2018-05-06 18:22:32 +0800

    修改模板

--- blog/views.py
+++ blog/views.py
@@ -9,25 +9,7 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
-
-
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    post.body = markdown.markdown(post.body, extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc'
-    ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list,
-    }
-    return render(request, 'blog/detail.html', context=context)
-
+    paginate_by = 10
 
 class PostDetailView(DetailView):
     model = Post
